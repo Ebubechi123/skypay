f86b751ca64ca45bb6fe23c4702676d37a89f21b
Ebubechi123 <ebube2003nice@gmail.com>
2022-01-08 02:34:52 +0100

    modified pages

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import Title from "../../Components/Title/Title"
+import "./Account.css"
 const Account = () => {
     return (
         <>
+        <div className="account-container">
+        <div className="title">
+          <Title>Your Account</Title>
+        </div>
+        </div>
         </>
       );
 }
